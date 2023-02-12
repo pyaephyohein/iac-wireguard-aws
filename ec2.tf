d82679f23e0f99b46f5c0598d3042698129eeb0d
pyaephyohein <pyaephyohein.info.3326@gmail.com>
2023-02-12 19:40:27 +0630

    terminate vpc and sg

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,9 @@
+
 resource "aws_instance" "wireguard" {
   ami           = "ami-055d15d9cfddf7bd3"
   instance_type = "t2.micro"
   key_name = var.ssh_key
+  associate_public_ip_address = true
   tags = {
     "Name"= "wireguard"
   }
@@ -10,13 +12,12 @@ resource "local_file" "ansible_inventory" {
 filename = "./ansible-wireguard/inventory"
 content = <<EOF
 [wireguard]
-hostname=wireguard ansible_ssh_host=${aws_instance.wireguard.public_ip} ansible_user=ubuntu
-[wireguard:vars]
+hostname=wireguard ansible_ssh_host=${aws_instance.wireguard.public_ip} ansible_user=ubuntu ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 ansible_python_interpreter = /usr/bin/python3
 ssh_args = -o StrictHostKeyChecking=no -oCompression=yes
 inventory = ./inventory
 EOF
 provisioner "local-exec" {command = "sleep 10 && ansible-playbook -i ./ansible-wireguard/inventory -v ./ansible-wireguard/run.yaml"}
-# provisioner "local-exec" {command = "rm -r ./ansible-wireguard/inventory"}
 }
 
+  
--- vpc.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_vpc" "wireguard" {
-  cidr_block = "10.0.0.0/16"
-}
